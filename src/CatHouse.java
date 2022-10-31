a654a19f307f978a7f48a9b75325c91d7c5f8f03
Alp Senay <alps2789@nycstudents.net>
2022-10-31 08:37:35 -0400

    a

--- src/CatHouse.java
+++ src/CatHouse.java
@@ -15,23 +15,44 @@ public class CatHouse {
     // introduce themselves alphabetically; if they have the same name AND
     // same age, then have cat1 go first
     public void rollCall() {
-        cat1.introduce();
-        cat2.introduce();
+        int ageDifference = cat1.getAge() - cat2.getAge();
+        int letterDifference = cat1.getName().compareTo(cat2.getName());
+        if(ageDifference == 0 && letterDifference == 0){
+            cat1.introduce();
+            cat2.introduce();
+        }
+        else if(ageDifference>0){
+            cat1.introduce();
+            cat2.introduce();
+        }
+        else if(ageDifference<0){
+            cat2.introduce();
+            cat1.introduce();
+        }
+        else if(letterDifference<0){
+            cat1.introduce();
+            cat2.introduce();
+        }
+        else{
+            cat2.introduce();
+            cat1.introduce();
+        }
     }
 
     // IMPLEMENT this method!
     // this method causes each cat to speak by printing
     // each Cat's returned "speak()" string to the console
     public void catNoise() {
-        /* implement me! */
+        System.out.println(cat1.speak());
+        System.out.println(cat2.speak());
     }
 
     // MODIFY this so that it returns a string with each Cat's AGE next to its name
     // e.g. Cat 1's Name: Petunia, Age: 3
     public String houseInfo() {
         String str = "Welcome to " + owner + "'s Cat House!\n";
-        str += "Cat 1's Name: " + cat1.getName() + "\n";
-        str += "Cat 2's Name: " + cat2.getName();
+        str += "Cat 1's Name: " + cat1.getName() + ", Age: " + cat1.getAge() + "\n";
+        str += "Cat 2's Name: " + cat2.getName() + ", Age: " + cat2.getAge();
         return str;
     }
 }
--- src/CatHouseRunner.java
+++ src/CatHouseRunner.java
@@ -7,20 +7,25 @@ public class CatHouseRunner {
         Cat cat2 = new Cat("Grumpy", 15);
 
         CatHouse house = new CatHouse(cat1, cat2, "Raymond");
-
-        System.out.println(house.houseInfo());
-        house.rollCall();
+//
+//        System.out.println(house.houseInfo());
+//        house.rollCall();
 
         // TASK 2-3: write the methods as specified, then write code below to
         // test your new methods using the objects you created above in Task 1
 
-
+//        System.out.println(cat1.getAge());
+//        System.out.println(cat1.speak());
+//
+//        house.rollCall();
+//        house.catNoise();
+//        System.out.println(house.houseInfo());
 
 
 
 
         // TASK 4: COMPLETE TEST CASES: uncomment the code below to fully test
-/*
+
     System.out.println("------- Test set 1 -------");
     Cat garfield = new Cat("Garfield", 5);
     Cat crookshanks = new Cat("Crookshanks", 6);
@@ -75,6 +80,6 @@ public class CatHouseRunner {
     sameCats.rollCall();
 
     sameCats.catNoise();
- */
+
     }
 }
